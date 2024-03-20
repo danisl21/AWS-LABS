fad9f33b06f753c721628f84e382aca726c672a0
danisl21 <danielaslima@yahoo.com>
2024-03-20 18:33:55 -0300

    lab3

---
+++ tipo_dados_string.py
@@ -0,0 +1,19 @@
+mystring="This is a string"
+print(mystring)
+
+print(type(mystring))
+
+print(mystring + " of the data type" + str(type(mystring)))
+
+firststring= "water"
+secondstring= "fall"
+thirdstring= firststring + secondstring
+print(thirdstring)
+
+name = input("What is your name?  ")
+print(name)
+
+color=input("What is your favorite color? ")
+animal=input("What is your favorite animal? ")
+
+print("{}, you like a {} {}!".format(name,color,animal))
