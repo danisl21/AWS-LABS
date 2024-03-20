dd87a3fd047e2c90388aff660cfb72b75bfff43e
danisl21 <danielaslima@yahoo.com>
2024-03-20 18:12:41 -0300

    lab2

---
+++ tipos_dados_numericos.py
@@ -0,0 +1,17 @@
+var = 2 + 2
+print(var)
+
+var = 4-2
+print(var)
+
+var= 2*2
+print(var)
+
+var=4/2
+print(var)
+
+quit()
+
+print("python has three numeric types: int, float, and complex")
+
+print ("oi")
---
+++ tipos_dados_numericos1.py
@@ -0,0 +1,32 @@
+print("oi")
+print("python has three numeric types:int,float,and complex")
+myvalue=1
+print("myvalue")
+
+print(type(myvalue))
+
+print(str(myvalue) + "is of the data type" + str(type(myvalue)))
+
+print("test")
+
+myvalue=3.14
+print(myvalue)
+
+print(type(myvalue))
+
+print(str(myvalue) + "is of the data type" + str(type(myvalue)))
+
+myvalue=5j
+print(myvalue)
+print(type(myvalue))
+print(str(myvalue) + "is of the data type" + str(type(myvalue)))
+
+myvalue=True
+print(myvalue)
+print(type(myvalue))
+
+myvalue=False
+print(myvalue)
+print(type(myvalue))
+
+
